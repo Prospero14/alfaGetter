ffbc2aa8b5cf9776d2f2e5fd1d64027edc81d78b
Prospero14 <39691151+Prospero14@users.noreply.github.com>
2020-01-16 13:18:30 -0800

    up
    up

--- src/main/java/alfaGetter/GetterGui.java
+++ src/main/java/alfaGetter/GetterGui.java
@@ -1,11 +1,26 @@
 package alfaGetter;
 
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.graalvm.compiler.phases.common.NodeCounterPhase;
+import org.w3c.dom.Text;
 
-public class GetterGui {
-    private JTextArea textArea1;
-    private JPanel panel1;
-    private JButton getPageButton;
-    private JButton quitButton;
+import java.awt.*;
+import java.io.InputStream;
+
+
+public class GetterGui extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Text helloWorld = new Text("Hello world");
+        StackPane root = new StackPane(helloWorld);
+        primaryStage.setScene(new Scene(root, 300, 120));
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(GetterGui.class, args);
+    }
 }
-
